609305ab402a296e37a5a31c911b7ed0c5fb76c2
chancho8 <94852144+chancho8@users.noreply.github.com>
2022-01-28 15:00:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import matplotlib.pyplot as plt
 
 df = pd.read_excel('2020-Make-and-model-data-(xls).xlsx')
 
-pd.set_option("display.max_columns", 60)
-##print(pd.get_option("display.max_columns"))
-##print(df.columns.tolist())
-
 birthYear = df['Year Of Birth'].to_numpy()
 failPctg = df['FAIL %'].to_numpy()
 passPctg = df['PASS %'].to_numpy()
@@ -23,5 +19,3 @@ for i in range(birthYear.size):
     if failPctg[i] >= 80:
         sum += 2020 - birthYear[i]
 mean = sum/(birthYear.size)
-print(mean)
-
